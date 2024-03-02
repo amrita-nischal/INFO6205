cdd409d688e165f0a0b0d9b5dbbe121d4853c6b7
Amrita Nischal <nischal.a@northeastern.edu>
2024-03-01 22:49:18 -0500

    Assignment 5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -20,10 +20,15 @@ public class Main {
         processArgs(args);
         System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[4000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+
+        int thread = (int)Math.pow(2,1);
+        ParSort.customPool = new ForkJoinPool(thread);
+        System.out.println("Number of threads: " +thread);
+
+        for (int j = 1; j <= 50; j++) {
+            ParSort.cutoff = 100000 * (j + 2);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
@@ -36,7 +41,7 @@ public class Main {
             timeList.add(time);
 
 
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10 times Time:" + time + "ms");
 
         }
         try {
